43cef2d6555c492d2d3161c7dcd4f61452fb806d
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-23 21:39:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,25 @@
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+# 
+#   def initialized(brand)
+#     @brand = brand
+#   end
+#   def cobble
+#     puts "Your shoe is as good as new!"
+#     @condition= "new"
+#   end
+# end
+
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialized(brand)
+  def initialize(brand)
     @brand = brand
   end
+
   def cobble
     puts "Your shoe is as good as new!"
     @condition= "new"
