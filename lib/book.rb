e03cd04ca49a7eace0836c2c54f23b0831d82888
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-23 20:41:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,7 +5,7 @@ class Book
   def initialize(title)
     @title = title
   end
-  
+
   def turn_page
     puts "Flippung the page...wow, you read fast!"
   end
@@ -18,34 +18,34 @@ end
 #   def initialize(title)
 #     @title = title
 #   end
-# 
+#
 #   def title
 #     @title
 #   end
-# 
+#
 #   def author=(author)
 #     @author = author
 #   end
-# 
+#
 #   def author
 #     @author
 #   end
-# 
+#
 #   def page_count=(num)
 #     @page_count = num
 #   end
-# 
+#
 #   def page_count
 #     @page_count
 #   end
-# 
+#
 #   def genre=(genre)
 #     @genre = genre
 #   end
 #   def genre
 #     @genre
 #   end
-# 
+#
 #   def turn_page
 #     @turn_page
 #     puts "Flipping the page...wow, you read fast!"
